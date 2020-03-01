6f547b46e814e7bbc77f52db8afc27d9e9a590a0
Moshe Dego <moshedego@gmail.com>
2020-03-01 09:52:05 +0200

    add error boundry support

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import { render } from 'react-dom';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 import Scroll from '../components/Scrool'
 
@@ -40,7 +40,9 @@ class App extends Component {
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filterRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filterRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
